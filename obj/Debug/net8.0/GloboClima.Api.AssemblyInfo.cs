f282131cfd412bf4aa740d3757f6e6bff739550b
Dayrone <dayrone.tomazetti@gmail.com>
2025-05-04 21:26:39 -0300

    Adiciona buildspec.yml para pipeline AWS

--- obj/Debug/net8.0/GloboClima.Api.AssemblyInfo.cs
+++ obj/Debug/net8.0/GloboClima.Api.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("GloboClima.Api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f1b7496517b975a5197aadfead61ce9f8fb8792c")]
 [assembly: System.Reflection.AssemblyProductAttribute("GloboClima.Api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("GloboClima.Api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
